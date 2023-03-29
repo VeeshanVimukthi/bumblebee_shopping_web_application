0a207b80c2650ec10b4ca4562a3a1094dab11a4f
VeeshanVimukthi <127432371+VeeshanVimukthi@users.noreply.github.com>
2023-03-30 02:40:31 +0530

    customer loging update

--- src/main/java/com/example/bumblebee/admin/UpdateServlet.java
+++ src/main/java/com/example/bumblebee/admin/UpdateServlet.java
@@ -10,6 +10,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Base64;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -23,66 +24,109 @@ import javax.servlet.http.Part;
 @MultipartConfig(maxFileSize = 16177215)    // upload file's size up to 16MB
 public class UpdateServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+//
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//
+//        // get form data
+//        String productId = request.getParameter("productId");
+//        String name = request.getParameter("name");
+//        String description = request.getParameter("description");
+//        String category = request.getParameter("category");
+//        BigDecimal price = new BigDecimal(request.getParameter("price"));
+//        String dimensions = request.getParameter("dimensions");
+//        String weight = request.getParameter("weight");
+//        String color = request.getParameter("color");
+//        InputStream inputStream = null; // input stream of the upload file
+//
+//        // obtains the upload file part in this multipart request
+//        Part filePart = request.getPart("image");
+//        if (filePart != null) {
+//            // obtains input stream of the upload file
+//            inputStream = filePart.getInputStream();
+//        }
+//
+//        // update product data in database
+//        String sql = "UPDATE products SET name = ?, description = ?, category = ?, price = ?, dimensions = ?, weight = ?, color = ? ";
+//        if (inputStream != null) {
+//            sql += ", image = ? ";
+//        }
+//        sql += "WHERE id = ?";
+//        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//            Connection conn = DBConnection.getConn();
+//            PreparedStatement statement = conn.prepareStatement(sql);
+//            statement.setString(1, name);
+//            statement.setString(2, description);
+//            statement.setString(3, category);
+//            statement.setBigDecimal(4, price);
+//            statement.setString(5, dimensions);
+//            statement.setString(6, weight);
+//            statement.setString(7, color);
+//
+//            if (inputStream != null) {
+//                // fetches input stream of the upload file for the blob column
+//                statement.setBlob(8, inputStream);
+//                statement.setString(9, productId);
+//            } else {
+//                statement.setString(8, productId);
+//            }
+//
+//            statement.executeUpdate();
+//            statement.close();
+//            conn.close();
+//        } catch (SQLException | ClassNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//
+//        // display success message in alert box
+//        response.setContentType("text/html;charset=UTF-8");
+//        response.getWriter().write("<script>alert('Product updated successfully!')</script>");
+//
+//        // redirect to product list page
+//        response.sendRedirect("./Admin/edit.jsp?id=" + productId);
+//    }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
-        // get form data
-        String productId = request.getParameter("productId");
+        // get product ID and updated information from the form
+        int productId = Integer.parseInt(request.getParameter("productId"));
         String name = request.getParameter("name");
         String description = request.getParameter("description");
         String category = request.getParameter("category");
         BigDecimal price = new BigDecimal(request.getParameter("price"));
+        byte[] image = Base64.getDecoder().decode(request.getParameter("image"));
         String dimensions = request.getParameter("dimensions");
         String weight = request.getParameter("weight");
         String color = request.getParameter("color");
-        InputStream inputStream = null; // input stream of the upload file
 
-        // obtains the upload file part in this multipart request
-        Part filePart = request.getPart("image");
-        if (filePart != null) {
-            // obtains input stream of the upload file
-            inputStream = filePart.getInputStream();
-        }
-
-        // update product data in database
-        String sql = "UPDATE products SET name = ?, description = ?, category = ?, price = ?, dimensions = ?, weight = ?, color = ? ";
-        if (inputStream != null) {
-            sql += ", image = ? ";
-        }
-        sql += "WHERE id = ?";
+        // update the product in the database
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
             Connection conn = DBConnection.getConn();
-            PreparedStatement statement = conn.prepareStatement(sql);
+            PreparedStatement statement = conn.prepareStatement(
+                    "UPDATE products SET name=?, description=?, category=?, price=?, image=?, dimensions=?, weight=?, color=? WHERE id=?");
             statement.setString(1, name);
             statement.setString(2, description);
             statement.setString(3, category);
             statement.setBigDecimal(4, price);
-            statement.setString(5, dimensions);
-            statement.setString(6, weight);
-            statement.setString(7, color);
-
-            if (inputStream != null) {
-                // fetches input stream of the upload file for the blob column
-                statement.setBlob(8, inputStream);
-                statement.setString(9, productId);
-            } else {
-                statement.setString(8, productId);
-            }
-
+            statement.setBytes(5, image);
+            statement.setString(6, dimensions);
+            statement.setString(7, weight);
+            statement.setString(8, color);
+            statement.setInt(9, productId);
             statement.executeUpdate();
             statement.close();
             conn.close();
-        } catch (SQLException | ClassNotFoundException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        // display success message in alert box
-        response.setContentType("text/html;charset=UTF-8");
-        response.getWriter().write("<script>alert('Product updated successfully!')</script>");
-
-        // redirect to product list page
-        response.sendRedirect("./Admin/edit.jsp?id=" + productId);
+        // redirect to the same page to refresh the list of products
+        response.sendRedirect(request.getContextPath() + "/view-products");
     }
+
+
+
+
 }
--- src/main/webapp/Admin/update.jsp
+++ src/main/webapp/Admin/update.jsp
@@ -14,34 +14,66 @@
 <%@include file="admin_navigation.jsp"%>
 
 
-<h1>Update Product</h1>
-<form action="UpdateProductServlet" method="post" enctype="multipart/form-data">
-    <input type="hidden" name="id" value="${product.id}">
+<%--<h1>Update Product</h1>--%>
+<%--<form action="UpdateProductServlet" method="post" enctype="multipart/form-data">--%>
+<%--    <input type="hidden" name="id" value="${product.id}">--%>
+<%--    <label for="name">Name:</label>--%>
+<%--    <input type="text" name="name" value="${product.name}">--%>
+<%--    <br><br>--%>
+<%--    <label for="description">Description:</label>--%>
+<%--    <textarea name="description">${product.description}</textarea>--%>
+<%--    <br><br>--%>
+<%--    <label for="category">Category:</label>--%>
+<%--    <input type="text" name="category" value="${product.category}">--%>
+<%--    <br><br>--%>
+<%--    <label for="price">Price:</label>--%>
+<%--    <input type="text" name="price" value="${product.price}">--%>
+<%--    <br><br>--%>
+<%--    <label for="image">Image:</label>--%>
+<%--    <input type="file" name="image">--%>
+<%--    <br><br>--%>
+<%--    <label for="dimensions">Dimensions:</label>--%>
+<%--    <input type="text" name="dimensions" value="${product.dimensions}">--%>
+<%--    <br><br>--%>
+<%--    <label for="weight">Weight:</label>--%>
+<%--    <input type="text" name="weight" value="${product.weight}">--%>
+<%--    <br><br>--%>
+<%--    <label for="color">Color:</label>--%>
+<%--    <input type="text" name="color" value="${product.color}">--%>
+<%--    <br><br>--%>
+<%--    <input type="submit" value="Update Product">--%>
+<%--</form>--%>
+
+<form method="post" action="<%= request.getContextPath() %>/view-products">
+    <input type="hidden" name="productId" value="<%= product.getId() %>">
     <label for="name">Name:</label>
-    <input type="text" name="name" value="${product.name}">
-    <br><br>
+    <input type="text" name="name" value="<%= product.getName() %>">
+    <br>
     <label for="description">Description:</label>
-    <textarea name="description">${product.description}</textarea>
-    <br><br>
+    <textarea name="description"><%= product.getDescription() %></textarea>
+    <br>
     <label for="category">Category:</label>
-    <input type="text" name="category" value="${product.category}">
-    <br><br>
+    <input type="text" name="category" value="<%= product.getCategory() %>">
+    <br>
     <label for="price">Price:</label>
-    <input type="text" name="price" value="${product.price}">
-    <br><br>
+    <input type="text" name="price" value="<%= product.getPrice() %>">
+    <br>
     <label for="image">Image:</label>
     <input type="file" name="image">
-    <br><br>
+    <br>
     <label for="dimensions">Dimensions:</label>
-    <input type="text" name="dimensions" value="${product.dimensions}">
-    <br><br>
+    <input type="text" name="dimensions" value="<%= product.getDimensions() %>">
+    <br>
     <label for="weight">Weight:</label>
-    <input type="text" name="weight" value="${product.weight}">
-    <br><br>
+    <input type="text" name="weight" value="<%= product.getWeight() %>">
+    <br>
     <label for="color">Color:</label>
-    <input type="text" name="color" value="${product.color}">
-    <br><br>
-    <input type="submit" value="Update Product">
+    <input type="text" name="color" value="<%= product.getColor() %>">
+    <br>
+    <button type="submit" name="action" value="update">Update</button>
 </form>
+
+
+
 </body>
 </html>
